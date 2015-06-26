53a07606c32d3a01d9d0a7dea0d7ab79e8babc40
Shrey Arora <shreyarora.15@gmail.com>
2015-06-26 20:46:50 +0530

    document download complete

--- lexeme-web/src/main/java/com/lexeme/admin/web/service/IDocumentAdminService.java
+++ lexeme-web/src/main/java/com/lexeme/admin/web/service/IDocumentAdminService.java
@@ -2,15 +2,14 @@ package com.lexeme.admin.web.service;
 
 import java.util.List;
 
+import com.lexeme.web.domain.document.DocumentDB;
 import com.lexeme.web.pojo.document.DocumentPojo;
 
 public interface IDocumentAdminService {
 
 	public List<DocumentPojo> getUnverifiedDocuments(int perPage);
-	
 	public String deleteDocument(Long documentId);
-	
 	public DocumentPojo getDocumentFromDocumentId(Long documentId);
-	
 	public String saveDocument(DocumentPojo documentPojo);
+	public DocumentDB getDocumentById(Long documentId);
 }
--- lexeme-web/src/main/java/com/lexeme/web/service/document/IDocumentService.java
+++ lexeme-web/src/main/java/com/lexeme/web/service/document/IDocumentService.java
@@ -5,4 +5,5 @@ import org.springframework.web.multipart.MultipartFile;
 public interface IDocumentService {
 
 	public String uploadFile(String category, String courseId, String description, MultipartFile file);
+	public String getFilePathOfDocumentID(Long documentId);
 }
