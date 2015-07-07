e7b1cb4e3e345b99f3ed7ed858a2572190b04a75
Shrey Arora <shreyarora.15@gmail.com>
2015-07-08 00:48:24 +0530

    changes done in dispatcher servlet

--- studydoer/src/main/java/com/sd/web/listener/StudyDoerStartupListener.java
+++ studydoer/src/main/java/com/sd/web/listener/StudyDoerStartupListener.java
@@ -11,9 +11,9 @@ import org.apache.log4j.Logger;
 
 import com.sd.web.util.PropertiesUtil;
 
-public class LexemeStartupListener implements ServletContextListener{
+public class StudyDoerStartupListener implements ServletContextListener{
 
-	private static final Logger logger = Logger.getLogger(LexemeStartupListener.class);
+	private static final Logger logger = Logger.getLogger(StudyDoerStartupListener.class);
 	
 	@Override
 	public void contextDestroyed(ServletContextEvent arg0) {
@@ -22,18 +22,18 @@ public class LexemeStartupListener implements ServletContextListener{
 
 	@Override
 	public void contextInitialized(ServletContextEvent arg0) {
-		logger.info("Lexeme Startup Servlet Listener Started");
+		logger.info("Study Doer Startup Servlet Listener Started");
 		
 		String profile = System.getProperty("platform_name");
 		String projectProFileName = profile + "/project.properties";
 		PropertiesUtil.setProjectProperties(getProperties("/" + projectProFileName));
-		logger.info("Lexeme Startup Servlet Listener Completed");
+		logger.info("Study Doer Startup Servlet Listener Completed");
 	}
 	
 	private Properties getProperties(String fileName) {
 		
 		Properties properties = new Properties();
-		InputStream fis = LexemeStartupListener.class.getResourceAsStream(fileName);
+		InputStream fis = StudyDoerStartupListener.class.getResourceAsStream(fileName);
 		try {
 			properties.load(fis);
 		} catch (IOException ex) {
